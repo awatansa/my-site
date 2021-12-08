90095c552bbf6c326cdcd0225ada1851d7891a40
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-12-08 23:00:29 +0530

    Bug Fix

--- components/ChatApp.jsx
+++ components/ChatApp.jsx
@@ -2,15 +2,8 @@ import {useSession} from "next-auth/client";
 import styles from "styles/Chat.module.css"
 
 export default function ChatApp() {
-    const session =
-    {
-        "user": {
-            "name": "Awatansa Vishwakarma",
-            "email": "ravi.awatansa@gmail.com",
-            "image": "https://lh3.googleusercontent.com/a-/AOh14GgDgf1QQ5yjRXhI4xWu7Y6dx7HxPSbYrCVTI1D8RgU=s96-c"
-        },
-        "expires": "2022-01-07T17:17:16.619Z"
-    }
+    const [session] = useSession()
+
     return (
         <div className={styles.chat}>
             {session && <h3>Welcome {session.user.name}!</h3>}
