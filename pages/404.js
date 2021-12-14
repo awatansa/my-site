df63d13a31cd1e425a6678f4f181ba43150b2df5
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-12-14 07:14:18 +0530

    Improved NotFound Page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,16 @@
 import { useRouter } from "next/router";
 import NotFound from "components/NotFound";
+import Head from "next/head";
 
 export default function NotFoundPage() {
   const router = useRouter();
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>404: Not Found</title>
+      </Head>
       <NotFound path={router.pathname} />
-    </div>
+    </>
   );
 }
