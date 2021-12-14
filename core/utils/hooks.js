b26585c1d89e476d1e606266d2e809b3a08172c7
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-12-15 01:29:39 +0530

    removed hook useTitle()

--- core/utils/hooks.js
+++ core/utils/hooks.js
@@ -1,18 +1,5 @@
-import { useEffect } from "react";
 import { useSession as useNextAuthSession } from "next-auth/react";
 
-export function useTitle(newTitle) {
-  let title;
-  if (process.browser) {
-    title = document.title;
-  }
-  useEffect(() => {
-    if (newTitle instanceof String) document.title = newTitle;
-    return () => {
-      document.title = title;
-    };
-  });
-}
 
 export function useFavicon() {
   // noinspection UnnecessaryLocalVariableJS
@@ -66,14 +53,13 @@ export function useSession() {
         user: {
           name: "Awatansa Vishwakarma",
           email: "ravi.awatansa@gmail.com",
-          image: "https://lh3.googleusercontent.com/a-/AOh14GgDgf1QQ5yjRXhI4xWu7Y6dx7HxPSbYrCVTI1D8RgU=s96-c",
+          image: "https://lh3.googleusercontent.com/a-/AOh14GgDgf1QQ5yjRXhI4xWu7Y6dx7HxPSbYrCVTI1D8RgU=s96-c"
         },
-        expires: "2022-01-09T17:19:38.745Z",
+        expires: "2022-01-09T17:19:38.745Z"
       },
-      status: "authenticated",
+      status: "authenticated"
     };
   }
 
   return { data, status };
-  l̥;
 }
