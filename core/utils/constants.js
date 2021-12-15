dfc132ed1638174f3924a482f574791950dff35c
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-12-15 09:41:46 +0530

    new Constants

--- core/utils/constants.js
+++ core/utils/constants.js
@@ -5,3 +5,8 @@ export const URL = {
   CHAT: "/chat",
   DECLARE: "/declaration",
 };
+export const DEV = "development"
+export const PROD = "production"
+export const isDev = process.env.NODE_ENV === DEV
+export const ADMIN = process.env.ADMIN
+export const MONGODB_URI = process.env.MONGODB_URI
