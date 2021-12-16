f3de5e49a080438fc60ca6c5f81b3318a8ebb6a7
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-12-17 01:45:52 +0530

    Removed Snap CSS Fixes #14

--- components/Index.jsx
+++ components/Index.jsx
@@ -1,14 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "styles/Home.module.css";
 
 export default function Index() {
-  const rootRef = useRef();
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
-  function handleScroll(e) {
-    // ref.current.scrollLeft += e.deltaY;
-  }
 
   async function loadData() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -25,7 +20,7 @@ export default function Index() {
   function Test({ data }) {
     return (
       <div
-        className={"flex flex-col justify-center gap-2 w-96 bg-slate-200 dark:bg-slate-700 snap-start shadow-md rounded p-2"}>
+        className={"flex flex-col justify-center gap-2 w-72 bg-slate-200 dark:bg-slate-700 shadow-md rounded p-2"}>
         <h3 className={"font-bold text-xl uppercase"}>{data.title}</h3>
         <p>{data.body}</p>
       </div>
@@ -34,11 +29,14 @@ export default function Index() {
 
   return (
     <div className={"inline-block h-full w-full overflow-hidden"}>
-      <div ref={rootRef}
-           className={`${classes.scroll} flex flex-col flex-wrap gap-2 justify-between overflow-x-auto overflow-y-hidden scroll-smooth snap-x w-full h-full pb-2`}
-           onWheel={handleScroll}>
-        {isLoading ? <h3 className={"text-2xl"}>Loading...</h3> : data.map((item, index) => <Test key={index}
-                                                                                                  data={item} />)}
+      <div
+        className={`${classes.scroll} flex flex-col flex-wrap gap-2 justify-between overflow-x-auto overflow-y-hidden w-full h-full pb-2`}
+      >
+        {isLoading ?
+          (<h3 className={"text-2xl"}>Loading...</h3>)
+          : data.map((item, index) => (
+            <Test key={index} data={item} />
+          ))}
       </div>
     </div>
   );
