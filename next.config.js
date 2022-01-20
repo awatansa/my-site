7a93be1b0f070455ef9af13c6d41e5f535584e95
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2022-01-21 05:01:20 +0530

    generateBuildId()

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 
 module.exports = {
   reactStrictMode: true,
-  images:{
-    domains:["lh3.googleusercontent.com"]
+  images: {
+    domains: ["lh3.googleusercontent.com"]
+  },
+  generateBuildId: async () => {
+    return require("uuid").v4();
   }
 };
